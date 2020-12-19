63e50ac92cdd86d17c5f40bfe6296b9d4648d6ba
Shaoting Cai <caishaoting@gmail.com>
2020-12-18 22:18:32 -0800

    script-based rendering proto type

--- public/tab-maker.js
+++ public/tab-maker.js
@@ -1,12 +1,55 @@
 function tabMakerMain() {
   console.log('Tab Maker');
+  const tabScript = document.getElementById('tab-script');
+  tabScript.addEventListener('change', (event) => {
+    renderTab(event.target.value);
+  });
+}
+
+function getMatchOrNull(match) {
+  return (match && match.length > 1) ? match[1] : null;
+}
+
+function renderBlock(tabRoot, b) {
+  if (!b || b.length == 0) {
+    return;
+  }
+  const chord = getMatchOrNull(b.match(/\[(.+)\]/));
+  const pitch = getMatchOrNull(b.match(/\((.+)\)/));
+  const lyrics = getMatchOrNull(b.match(/`(.+)`/));;
+  console.log(`Chord: ${chord}, Pitch: ${pitch}, Lyrics: ${lyrics}`);
+  const newBlock = new TabMakerBlock();
+  if (chord) {
+    newBlock.setAttribute('chord', chord);
+  }
+  if (pitch) {
+    newBlock.setAttribute('pitch', pitch);
+  }
+  if (lyrics) {
+    newBlock.setAttribute('lyrics', lyrics);
+  }
+  tabRoot.appendChild(newBlock);
+}
+
+function renderMeasure(tabRoot, m) {
+  const blocks = m.split(',');
+  blocks.map(b => b.trim())
+    .forEach(b => renderBlock(tabRoot, b));
+  tabRoot.appendChild(document.createElement("tab-maker-pipe"));
+}
+
+function renderTab(script) {
+  const tabRoot = document.getElementById('tab-root');
+  tabRoot.innerHTML = '';
+  const measures = script.split('|');
+  measures.forEach(m => renderMeasure(tabRoot, m));
 }
 
 class TabMakerBlock extends HTMLElement {
   constructor() {
-      super();
-      this.root = this.attachShadow({ mode: 'open' });
-      this.root.innerHTML = `
+    super();
+    this.root = this.attachShadow({ mode: 'open' });
+    this.root.innerHTML = `
       <style>
         .tm-block {
 
@@ -18,35 +61,22 @@ class TabMakerBlock extends HTMLElement {
           height: 100%;
         }
       </style>
-      <div class="tm-block">
-        <div class="tm-chord">
-          <input type="text">
-        </div>
-        <div class="tm-pitch"></div>
-        <div class="tm-lyrics"></div>
-      </div>
+      <div class="tm-chord"></div>
+      <div class="tm-pitch"></div>
+      <div class="tm-lyrics"></div>
       `;
   }
 
   connectedCallback() {
-      this.chordDiv = this.root.querySelector(".tm-chord");
-      this.pitchDiv = this.root.querySelector(".tm-pitch");
-      this.lyricsDiv = this.root.querySelector(".tm-lyrics");
-      this.blockDiv = this.root.querySelector(".tm-block");
-      this.chord = this.getAttribute("chord");
-      this.pitch = this.getAttribute("pitch");
-      this.lyrics = this.getAttribute("lyrics");
-      this.chordDiv.textContent = this.chord;
-      this.pitchDiv.textContent = this.pitch;
-      this.lyricsDiv.textContent = this.lyrics;
-
-      this.chordDiv.addEventListener('click', (event) => {
-        event.target.style.background = 'pink';
-      });
-      
-      this.chordDiv.addEventListener('blur', (event) => {
-        event.target.style.background = '';
-      });
+    this.chordDiv = this.root.querySelector(".tm-chord ");
+    this.pitchDiv = this.root.querySelector(".tm-pitch ");
+    this.lyricsDiv = this.root.querySelector(".tm-lyrics ");
+    this.chord = this.getAttribute("chord");
+    this.pitch = this.getAttribute("pitch");
+    this.lyrics = this.getAttribute("lyrics");
+    this.chordDiv.textContent = this.chord;
+    this.pitchDiv.textContent = this.pitch;
+    this.lyricsDiv.textContent = this.lyrics;
   }
 }
 
